b325cdddc273348ced50de80bc1b7d24dd628fed
daniele <grignani.d@gmail.com>
2016-06-08 22:29:02 +0200

    Updated

---
+++ rat-wsserver/src/main/java/com/dgr/rat/auth/db/IncorrectCredentialsException.java
@@ -0,0 +1,15 @@
+/**
+ * @author: Daniele Grignani
+ * @date: Jun 6, 2016
+ */
+
+package com.dgr.rat.auth.db;
+
+/**
+ * @author dgr
+ *
+ */
+public class IncorrectCredentialsException extends Exception{
+	private static final long serialVersionUID = 1L;
+
+}
